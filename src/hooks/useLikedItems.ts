2ebaa485e4ec6ece53eb4a0c356015f85dc3b4a0
joshfisidi <josh@fisidi.com>
2024-11-13 00:51:58 -0500

    removed auth for likes

--- src/hooks/useLikedItems.ts
+++ src/hooks/useLikedItems.ts
@@ -1,7 +1,6 @@
 "use client"
 
 import { useState, useEffect } from 'react'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 
 export function useLikedItems() {
   const [likedItems, setLikedItems] = useState<string[]>([])
